92bb1999f56214655bbf58b845e71cc86db670af
PeterBrady84 <scobbybrady@gmail.com>
2015-04-10 16:20:38 +0100

    Patches

---
+++ src/model/TimeSlot.java
@@ -0,0 +1,32 @@
+package model;
+
+/**
+ * Created by Peter on 08/04/2015.
+ */
+public class TimeSlot {
+
+    private int timeSlotId;
+    private String timeDesc;
+
+    public TimeSlot(int i, String d) {
+        System.out.println("Inside : timeSlotModel");
+        this.timeSlotId = i;
+        this.timeDesc = d;
+    }
+
+    public int getTimeSlotId() {
+        return timeSlotId;
+    }
+
+    public void setTimeSlotId(int i) {
+        this.timeSlotId = i;
+    }
+
+    public String getTimeDesc() {
+        return timeDesc;
+    }
+
+    public void setTimeDesc(String t) {
+        this.timeDesc = t;
+    }
+}
